a3a7de845162f2fde7491c7eb790a6db0f9b550d
soam1 <soamakash9@gmail.com>
2023-11-13 09:33:52 +0530

    checking for device's location

--- app/src/main/java/com/akashsoam/mausam/MainActivity.kt
+++ app/src/main/java/com/akashsoam/mausam/MainActivity.kt
@@ -1,11 +1,27 @@
-package com.akashsoam.mausam
+    package com.akashsoam.mausam
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(!isLocationEnabled()){
+            Toast.makeText(this@MainActivity, "Please enable the device's Location", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this@MainActivity, "The location is already enabled", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun isLocationEnabled():Boolean{
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
