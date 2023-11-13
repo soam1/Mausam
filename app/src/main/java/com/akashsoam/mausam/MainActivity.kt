6e6375e396bc6c01ff56cc13eb314d1729064bfa
soam1 <soamakash9@gmail.com>
2023-11-13 10:45:12 +0530

    requested permissions

--- app/src/main/java/com/akashsoam/mausam/MainActivity.kt
+++ app/src/main/java/com/akashsoam/mausam/MainActivity.kt
@@ -1,27 +1,131 @@
-    package com.akashsoam.mausam
+package com.akashsoam.mausam
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
-    class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_LOCATION_CODE = 123123
+    private lateinit var _mFusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(!isLocationEnabled()){
-            Toast.makeText(this@MainActivity, "Please enable the device's Location", Toast.LENGTH_SHORT).show()
+
+        _mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this@MainActivity,
+                "Please enable the device's Location",
+                Toast.LENGTH_SHORT
+            ).show()
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
-        }else{
-            Toast.makeText(this@MainActivity, "The location is already enabled", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissions()
         }
     }
-    private fun isLocationEnabled():Boolean{
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_CODE && grantResults.isNotEmpty()) {
+            Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+            requestLocationData()
+        } else {
+            Toast.makeText(this@MainActivity, "Permission NOT Granted", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val locationRequest = com.google.android.gms.location.LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            1000
+        ).build()
+        _mFusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+//                val lastLocationLatitude = _mFusedLocationClient.lastLocation
+//                    .addOnSuccessListener { location: Location ->
+//                        val latitude = location.latitude
+//                    }
+//                val lastLocationLongitude = _mFusedLocationClient.lastLocation
+//                    .addOnSuccessListener { location: Location ->
+//                        val longitude = location.longitude
+//                    }
+                Toast.makeText(
+                    this@MainActivity,
+                    "latitude: ${locationResult.lastLocation?.latitude} \n longitude: ${locationResult.lastLocation?.longitude}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, Looper.myLooper())
+    }
+
+    private fun isLocationEnabled(): Boolean {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            showRequestDialog()
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            requestPermissions()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                REQUEST_LOCATION_CODE
+            )
+        }
+    }
+
+    private fun showRequestDialog() {
+        AlertDialog.Builder(this).setPositiveButton("GO TO SETTINGS") { _, _ ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }.setNegativeButton("CLOSE") { dialog, _ -> dialog.cancel() }
+            .setTitle("Location permission required")
+            .setMessage("go to settings to turn on the device location?").setCancelable(false)
+            .show()
+
     }
 }
